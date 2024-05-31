710bc5c71e34d9dcc3c23b93ddd9644bfe13152d
kanhupasayat <111569616+kanhupasayat@users.noreply.github.com>
2024-05-31 17:11:05 +0530

    Initial commit

--- studentacc/views.py
+++ studentacc/views.py
@@ -14,6 +14,11 @@ from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
 
 
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import StudentInfo
+
 
 
 
@@ -147,10 +152,7 @@ def student_info_form(request):
     return render(request,"student_info_form.html")
 
 
-from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import login_required
-from django.contrib import messages
-from .models import StudentInfo
+
 
 
 @login_required
